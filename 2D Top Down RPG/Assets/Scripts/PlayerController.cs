dd1fbc4141ee109d06c9ad98f3aa37ad58934172
Bahadır Sandıkçı <baho842@hotmail.com>
2025-10-27 00:15:12 +0300

    Add Idle , Running Anim and Mouse Follow

--- 2D Top Down RPG/Assets/Scripts/PlayerController.cs
+++ 2D Top Down RPG/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,11 +10,16 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAanimator;
+    private SpriteRenderer mySpriteRender;
+
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAanimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>(); 
     }
 
     private void OnEnable()
@@ -24,6 +30,8 @@ public class PlayerController : MonoBehaviour
     private void PlayerInput()
     {
         movement = playerControls.Player.Move.ReadValue<Vector2>();
+        myAanimator.SetFloat("moveX", movement.x);
+        myAanimator.SetFloat("moveY", movement.y);
     }
 
     private void Move()
@@ -38,6 +46,23 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection(); 
         Move();
-    }   
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRender.flipX = true;
+        }
+        else
+        {
+            mySpriteRender.flipX = false;
+        }
+
+    }
 }
