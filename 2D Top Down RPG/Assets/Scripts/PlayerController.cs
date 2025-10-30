495643ee8d37cd22065190517ccb689c8339dd60
Bahadır Sandıkçı <baho842@hotmail.com>
2025-10-30 19:20:14 +0300

    Sword Slash Anim Set

--- 2D Top Down RPG/Assets/Scripts/PlayerController.cs
+++ 2D Top Down RPG/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1f;
+public bool FacingLeft { get { return facingLeft; }  set { facingLeft = value; } } 
+
+[SerializeField] private float moveSpeed = 1f;
 
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator myAanimator;
     private SpriteRenderer mySpriteRender;
+    private bool facingLeft = false;
 
-
-    private void Awake()
+private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
@@ -58,11 +61,13 @@ public class PlayerController : MonoBehaviour
         if (mousePos.x < playerScreenPoint.x)
         {
             mySpriteRender.flipX = true;
-        }
+            FacingLeft = true;
+    }
         else
         {
             mySpriteRender.flipX = false;
-        }
+            FacingLeft = false;
+    }
 
     }
 }
--- 2D Top Down RPG/Assets/Scripts/Sword.cs
+++ 2D Top Down RPG/Assets/Scripts/Sword.cs
@@ -1,62 +1,143 @@
-// Gerekli Unity kütüphanelerini içeri aktarýr
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; // Unity'nin yeni Input System'ini kullanmak için bu satýr gerekli
 
-// 'Sword' adýnda yeni bir class (sýnýf) oluþturur. MonoBehaviour, bu script'in bir Unity objesine eklenebilmesini saðlar.
+// Bu script, kýlýç objesinin (veya kýlýcý tutan kolun) üzerinde olmalý.
 public class Sword : MonoBehaviour
 {
-    // PlayerControls (Unity Input System tarafýndan oluþturulan) sýnýfýndan bir deðiþken.
-    // Oyuncunun girdilerini (tuþ basýmlarý, mouse hareketleri vb.) yönetir.
+    // --- DEÐÝÞKENLER ---
+
+    [SerializeField] private GameObject slashAnimPrefab;
+    [SerializeField] private Transform slashAnimSpawnPoint;
+
+    // Oyuncu girdilerini (örn. "Attack" butonu) yönetmek için referans.
     private PlayerControls playerControls;
 
-    // Animasyonlarý kontrol etmek için kullanýlacak Animator bileþeni.
+    // Kýlýcýn saldýrý animasyonunu oynatmak için Animator bileþeni.
     private Animator myAnimator;
 
-    // Script ilk yüklendiðinde veya obje aktif olduðunda SADECE BÝR KEZ çalýþýr.
-    // Genellikle bileþen referanslarýný atamak için kullanýlýr.
+    // Oyuncunun ana script'ine (PlayerController) eriþmek için referans.
+    // Muhtemelen oyuncunun pozisyonunu almak için kullanýlýyor.
+    private PlayerController playerController;
+
+    // Silahýn kendisini (veya silahý tutan pivot objeyi) döndürmek için
+    // ActiveWeapon script'ine referans.
+    private ActiveWeapon activeWeapon;
+
+    private GameObject slashAnim; 
+    // --- UNITY METOTLARI ---
+
+    // Script ilk yüklendiðinde (Start'tan önce) çalýþýr.
+    // Genellikle referanslarý atamak için kullanýlýr.
     private void Awake()
     {
-        // Bu script'in eklendiði objenin üzerindeki Animator bileþenini bulur ve 'myAnimator' deðiþkenine atar.
+        // Bu objenin Parent (Ebeveyn) objelerinden PlayerController script'ini bul ve ata.
+        playerController = GetComponentInParent<PlayerController>();
+
+        // Bu objenin Parent objelerinden ActiveWeapon script'ini bul ve ata.
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+
+        // Bu objenin üzerindeki Animator bileþenini bul ve ata.
         myAnimator = GetComponent<Animator>();
 
-        // Yeni bir PlayerControls nesnesi oluþturur (Input System'i kullanýma hazýrlar).
+        // Yeni bir PlayerControls nesnesi oluþturarak giriþ sistemini baþlat.
         playerControls = new PlayerControls();
     }
 
-    // Bu script (veya baðlý olduðu obje) aktif hale geldiðinde çalýþýr.
+    // Obje veya script aktif olduðunda çalýþýr.
     private void OnEnable()
     {
-        // Oyuncu girdilerini dinlemeyi aktifleþtirir.
+        // Girdi eylemlerini (PlayerControls) dinlemeyi aktifleþtir.
         playerControls.Enable();
     }
 
-    // Ýlk frame güncellemesinden hemen önce SADECE BÝR KEZ çalýþýr.
-    // Genellikle diðer script'lerle baðlantý kurmak veya baþlangýç ayarlarý için kullanýlýr.
+    // Obje veya script devre dýþý kaldýðýnda çalýþýr.
+    private void OnDisable()
+    {
+        // Girdi eylemlerini dinlemeyi durdur (performans ve hata önleme için).
+        // playerControls.Combat.Attack.started -= _ => Attack(); // Olay aboneliðini de kaldýrmak daha güvenlidir.
+        playerControls.Disable();
+    }
+
+    // Ýlk frame güncellemesinden hemen önce çalýþýr.
     void Start()
     {
-        // 'playerControls' içindeki 'Combat' (Dövüþ) eylem haritasýndaki 'Attack' (Saldýrý) eylemini dinler.
-        // 'started' olayý, oyuncu bu eylemi baþlattýðý anda (örn. tuþa bastýðý anda) tetiklenir.
-        // Tetiklendiðinde, 'Attack()' metodunu çaðýrýr. 
-        // `_ =>` kýsmý, "olaydan gelen parametreleri (varsa) umursama, sadece metodu çaðýr" anlamýna gelir.
+        // playerControls içindeki "Combat" eylem haritasýndaki "Attack" eylemini dinle.
+        // Eylem "started" (baþladýðýnda, örn. tuþa basýldýðýnda) olduðunda, 'Attack()' metodunu çaðýr.
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
-    // Saldýrý eylemi gerçekleþtiðinde çaðrýlacak olan özel metot.
-    private void Attack()
+    // Her frame (kare) çalýþýr.
+    private void Update()
     {
-        // Animator bileþenine "Attack" isminde bir trigger (tetikleyici) gönderir.
-        // Bu, Animator'da tanýmlý olan saldýrý animasyonunu baþlatýr.
-        myAnimator.SetTrigger("Attack");
+        // Her frame, silahýn fareyi takip etmesini saðlayan metodu çaðýr.
+        MouseFollowWithOffset();
     }
 
-    // Not: Kodda OnDisable() metodu eksik. 
-    // Objeyi veya script'i kapattýðýnýzda girdileri dinlemeyi durdurmak iyi bir pratiktir:
-    /*
-    private void OnDisable()
+    // --- ÖZEL METOTLAR ---
+
+    // Saldýrý eylemi tetiklendiðinde çaðrýlýr.
+    private void Attack()
     {
-        // Oyuncu girdilerini dinlemeyi devre dýþý býrakýr.
-        playerControls.Disable();
+        // Animator'a "Attack" isimli trigger'ý (tetikleyiciyi) gönder.
+        // Bu, animatördeki saldýrý animasyonunu baþlatýr.
+        myAnimator.SetTrigger("Attack");
+        slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity); 
+        slashAnim.transform.parent = this.transform.parent; 
     }
-    */
+
+    public void SwingUpFlipAnim()
+    {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler(-180, 0, 0);
+
+        if (playerController.FacingLeft)
+        {
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    public void SwingDownFlipAnim()
+    {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        if (playerController.FacingLeft)
+        {
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+
+    // Farenin pozisyonuna göre silahýn yönünü ayarlar.
+    private void MouseFollowWithOffset()
+    {
+        // --- DEÐÝÞÝKLÝK BURADA ---
+        // Eski "Input.mousePosition" yerine Yeni Input System'in kodunu kullanýyoruz.
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        // -------------------------
+
+        // Oyuncunun dünyadaki pozisyonunu, kameradan ekrandaki piksel pozisyonuna çevir.
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        // Farenin ekrandaki pozisyonu ile EKRANIN SOL ALT KÖÞESÝ (0,0) arasýndaki açýyý hesapla.
+        // DÝKKAT: Eðer açýyý oyuncuya göre hesaplamak istiyorsanýz,
+        // "Mathf.Atan2(mousePos.y, mousePos.x)" yerine
+        // "Mathf.Atan2(mousePos.y - playerScreenPoint.y, mousePos.x - playerScreenPoint.x)"
+        // kullanmanýz daha doðru bir davranýþ saðlayabilir.
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        // Eðer fare, oyuncunun ekran pozisyonunun solundaysa:
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            // ActiveWeapon objesini Y ekseninde 180 derece döndür (ters çevir) 
+            // ve Z ekseninde hesaplanan 'angle' açýsýný uygula.
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        }
+        else // Eðer fare saðdaysa:
+        {
+            // ActiveWeapon objesini normal tut (Y'de 0 derece)
+            // ve Z ekseninde hesaplanan 'angle' açýsýný uygula.
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
 }
