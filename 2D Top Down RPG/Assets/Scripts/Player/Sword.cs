812498a0c86aa7dfafb7f3477907726ad7d8d817
Bahadır Sandıkçı <baho842@hotmail.com>
2025-11-10 01:49:36 +0300

    Add sword swing sound and screen shake effects
    Integrated sword swing sound effects and AudioSource to the player and Sword script. Added Cinemachine impulse source to BlueSlime prefab and impulse listener to the camera for screen shake on impact. Updated UI frame sprite and meta, adjusted weapon collider layer, and registered 'Weapon' layer in TagManager.

--- 2D Top Down RPG/Assets/Scripts/Player/Sword.cs
+++ 2D Top Down RPG/Assets/Scripts/Player/Sword.cs
@@ -12,6 +12,12 @@ public class Sword : MonoBehaviour
     [SerializeField] private Transform slashAnimSpawnPoint;
     [SerializeField] private Transform weaponCollider;
 
+    // --- SES EFEKTÝ ÝÇÝN YENÝ DEÐÝÞKENLER ---
+    [Tooltip("Kýlýç sallama ses klibi")]
+    [SerializeField] private AudioClip swordSwingSound; // <-- YENÝ EKLENDÝ
+    private AudioSource myAudioSource; // <-- YENÝ EKLENDÝ
+    // ------------------------------------
+
     // Oyuncu girdilerini (örn. "Attack" butonu) yönetmek için referans.
     private PlayerControls playerControls;
 
@@ -19,18 +25,15 @@ public class Sword : MonoBehaviour
     private Animator myAnimator;
 
     // Oyuncunun ana script'ine (PlayerController) eriþmek için referans.
-    // Muhtemelen oyuncunun pozisyonunu almak için kullanýlýyor.
     private PlayerController playerController;
 
     // Silahýn kendisini (veya silahý tutan pivot objeyi) döndürmek için
     // ActiveWeapon script'ine referans.
     private ActiveWeapon activeWeapon;
 
-    private GameObject slashAnim; 
+    private GameObject slashAnim;
     // --- UNITY METOTLARI ---
 
-    // Script ilk yüklendiðinde (Start'tan önce) çalýþýr.
-    // Genellikle referanslarý atamak için kullanýlýr.
     private void Awake()
     {
         // Bu objenin Parent (Ebeveyn) objelerinden PlayerController script'ini bul ve ata.
@@ -44,6 +47,10 @@ public class Sword : MonoBehaviour
 
         // Yeni bir PlayerControls nesnesi oluþturarak giriþ sistemini baþlat.
         playerControls = new PlayerControls();
+
+        // --- SES EFEKTÝ ÝÇÝN YENÝ SATIR ---
+        // Sesi çalacak olan AudioSource bileþenini ana Player objesinden bul.
+        myAudioSource = GetComponentInParent<AudioSource>(); // <-- YENÝ EKLENDÝ
     }
 
     // Obje veya script aktif olduðunda çalýþýr.
@@ -56,16 +63,14 @@ public class Sword : MonoBehaviour
     // Obje veya script devre dýþý kaldýðýnda çalýþýr.
     private void OnDisable()
     {
-        // Girdi eylemlerini dinlemeyi durdur (performans ve hata önleme için).
-        // playerControls.Combat.Attack.started -= _ => Attack(); // Olay aboneliðini de kaldýrmak daha güvenlidir.
+        // Girdi eylemlerini dinlemeyi durdur.
         playerControls.Disable();
     }
 
     // Ýlk frame güncellemesinden hemen önce çalýþýr.
     void Start()
     {
-        // playerControls içindeki "Combat" eylem haritasýndaki "Attack" eylemini dinle.
-        // Eylem "started" (baþladýðýnda, örn. tuþa basýldýðýnda) olduðunda, 'Attack()' metodunu çaðýr.
+        // Attack eylemini dinle.
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
@@ -81,20 +86,31 @@ public class Sword : MonoBehaviour
     // Saldýrý eylemi tetiklendiðinde çaðrýlýr.
     private void Attack()
     {
-        // Animator'a "Attack" isimli trigger'ý (tetikleyiciyi) gönder.
-        // Bu, animatördeki saldýrý animasyonunu baþlatýr.
+        // Animasyonu tetikle
         myAnimator.SetTrigger("Attack");
+        // Silahýn çarpýþtýrýcýsýný (hasar vuran alaný) aktifleþtir
         weaponCollider.gameObject.SetActive(true);
 
-        slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity); 
-        slashAnim.transform.parent = this.transform.parent; 
+        // Kýlýç vuruþu "slash" efektini oluþtur (Instantiate)
+        slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
+        slashAnim.transform.parent = this.transform.parent;
+
+        // --- SES EFEKTÝ ÝÇÝN YENÝ KOD ---
+        // Ses klibi atanmýþsa ve AudioSource bulunmuþsa, sesi çal.
+        if (swordSwingSound != null && myAudioSource != null)
+        {
+            myAudioSource.PlayOneShot(swordSwingSound); // <-- YENÝ EKLENDÝ
+        }
+        // --------------------------------
     }
 
+    // Bu fonksiyon, animasyonun "DoneAttacking" event'i tarafýndan çaðrýlýr.
     public void DoneAttackingAnimEvent()
     {
         weaponCollider.gameObject.SetActive(false);
     }
 
+    // Bu fonksiyon, animasyonun "SwingUpFlip" event'i tarafýndan çaðrýlýr.
     public void SwingUpFlipAnimEvent()
     {
         slashAnim.gameObject.transform.rotation = Quaternion.Euler(-180, 0, 0);
@@ -105,6 +121,7 @@ public class Sword : MonoBehaviour
         }
     }
 
+    // Bu fonksiyon, animasyonun "SwingDownFlip" event'i tarafýndan çaðrýlýr.
     public void SwingDownFlipAnimEvent()
     {
         slashAnim.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
@@ -119,29 +136,23 @@ public class Sword : MonoBehaviour
     // Farenin pozisyonuna göre silahýn yönünü ayarlar.
     private void MouseFollowWithOffset()
     {
-        // --- DEÐÝÞÝKLÝK BURADA ---
-        // Eski "Input.mousePosition" yerine Yeni Input System'in kodunu kullanýyoruz.
+        // Yeni Input System ile mouse pozisyonunu al
         Vector3 mousePos = Mouse.current.position.ReadValue();
-        // -------------------------
 
-        // Oyuncunun dünyadaki pozisyonunu, kameradan ekrandaki piksel pozisyonuna çevir.
+        // Oyuncunun dünyadaki pozisyonunu ekrandaki piksel pozisyonuna çevir.
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
 
-        // Farenin ekrandaki pozisyonu ile EKRANIN SOL ALT KÖÞESÝ (0,0) arasýndaki açýyý hesapla.
+        // Açý hesaplamasý
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 
-        // Eðer fare, oyuncunun ekran pozisyonunun solundaysa:
+        // Fare, oyuncunun solundaysa
         if (mousePos.x < playerScreenPoint.x)
         {
-            // ActiveWeapon objesini Y ekseninde 180 derece döndür (ters çevir) 
-            // ve Z ekseninde hesaplanan 'angle' açýsýný uygula.
             activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
             weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
         }
-        else // Eðer fare saðdaysa:
+        else // Fare, oyuncunun saðýndaysa
         {
-            // ActiveWeapon objesini normal tut (Y'de 0 derece)
-            // ve Z ekseninde hesaplanan 'angle' açýsýný uygula.
             activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
             weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
